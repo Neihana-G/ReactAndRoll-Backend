d043a55e2ec118a84f00109505a829290ef6d383
Toby Langdon <tobyjoelangdon@gmail.com>
2023-10-10 12:21:01 +1300

    First Commit

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require('express')
 const mysql = require('mysql2')
+const cors = require('cors')
 const {getPool} = require('./db/getPool')
 const homePageRoute = require('./routes/homePageTest')
+const helpRequests = require('./routes/helpRequests')
 require('dotenv').config()
 const app = express()
 
 port = process.env.PORT || 4000;
+
+//------ MIDDLEWARE ------//
+app.use(express.json())
+// app.use(cors);
 
 //---- DB CONNECTION ----//
 const pool = getPool()
 
 
 //------ ROUTES ------//
-app.use(homePageRoute)  //Example route to ensure working - can delete later
-
+app.get('/', (req, res) => {
+    res.send('Testing')
+})
+// app.use(homePageRoute)  //Example route to ensure working - can delete later
+app.use(helpRequests) 
 
 app.listen(port, () => {
     console.log(`Server live at http://localhost:${port}`)
