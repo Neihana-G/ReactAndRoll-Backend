e2f85b1772620a65ff8980bc5ee2d8b7a830e0f1
Toby Langdon <tobyjoelangdon@gmail.com>
2023-10-10 12:41:18 +1300

    Added cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ port = process.env.PORT || 4000;
 
 //------ MIDDLEWARE ------//
 app.use(express.json())
-// app.use(cors);
+app.use(cors());
 
 //---- DB CONNECTION ----//
 const pool = getPool()
