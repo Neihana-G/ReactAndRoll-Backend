971fc19ee7ad00e98d4c3d22440d899e7179d35e
Jin Zhang <jinzh235@gmail.com>
2023-10-10 16:56:05 +1300

    create branch

--- db/getPool.js
+++ db/getPool.js
@@ -1,18 +1,17 @@
-const mysql = require('mysql2')
-require('dotenv').config()
+const mysql = require("mysql2");
+require("dotenv").config();
 
 module.exports = {
-    getPool: () => {
-        return mysql.createPool({
-
-            host: process.env.MYSQL_HOST,
-            user: process.env.MYSQL_USER,
-            password: process.env.MYSQL_PASS,
-            database: process.env.MYSQL_DATABASE,
-            waitForConnections: true,
-            connectionLimit: 10,
-            queueLimit: 0,
-              
-        })
-    }
-}
+  getPool: () => {
+    return mysql.createPool({
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASS,
+      database: process.env.MYSQL_DATABASE,
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0,
+    });
+  },
+};
+// Path: ReactAndRoll-Backend/db/getPool.js
